69d216eedc42d1e08546316e1782a3b9d2222f9e
Cameron Warner <cameron.w.warner@gmail.com>
2023-11-13 20:14:08 -0500

    Initial Commit

---
+++ Better Rest/Better_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_RestApp.swift
+//  Better Rest
+//
+//  Created by Cameron Warner on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Better_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
