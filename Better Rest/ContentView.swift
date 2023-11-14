18db4cf417c8609c996680d468062d5ff64104e7
Cameron Warner <cameron.w.warner@gmail.com>
2023-11-13 21:01:45 -0500

    created State var sleepAmount with a Stepper

--- Better Rest/ContentView.swift
+++ Better Rest/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 0...13, step: 0.25)
         }
         .padding()
     }
---
+++ Better Rest/LogicModel.swift
@@ -0,0 +1,10 @@
+//
+//  LogicModel.swift
+//  Better Rest
+//
+//  Created by Cameron Warner on 11/13/23.
+//
+
+import SwiftUI
+import Foundation
+
